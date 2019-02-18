ed5f6b1331e057eca6951b14aaec918fd36c9de2
Sorin Chis <kish.sorin@yahoo.com>
2019-02-18 14:20:26 +0200

    dummy data imported in App component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import DummyData from './dummy-data';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <h1>INSTACLONE</h1>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			dummyData: []
+		};
+	}
+
+	componentDidMount() {
+		this.setState({
+			dummyData: DummyData
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<h1>INSTACLONE</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
