32a8a4642d257b39b3d6fbff12cb218238b86dc6
Sorin Chis <kish.sorin@yahoo.com>
2019-02-21 14:10:13 +0200

    added styled components to Post Container

--- instagram/src/components/PostContainer/Post.jsx
+++ instagram/src/components/PostContainer/Post.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const PostHeader = styled.div`
+	display: flex;
+	align-items: center;
+
+	img {
+		border-radius: 50%;
+		height: 40px;
+		margin: 8px;
+	}
+	p {
+		font-weight: bold;
+		font-size: italic;
+		font-size: 16px;
+		margin: 6px;
+	}
+`;
+
+const PostImg = styled.div`
+	img {
+		max-width: 100%;
+		height: auto;
+	}
+`;
 
 const Post = (props) => {
 	return (
 		<div>
-			<div className="post-header">
+			<PostHeader>
 				<img src={props.thumbnailUrl} alt="user-img" />
 				<p>{props.username}</p>
-			</div>
+			</PostHeader>
 
-			<div className="post-img">
+			<PostImg>
 				<img src={props.imageUrl} alt="post-img" />
-			</div>
+			</PostImg>
 		</div>
 	);
 };
--- instagram/src/components/PostContainer/PostContainer.jsx
+++ instagram/src/components/PostContainer/PostContainer.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import './PostContainer.css';
+//import './PostContainer.css';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection/CommentSection';
 import Post from './Post';
 
+const PostDiv = styled.div`
+	width: 44%;
+	margin: 15px auto;
+	border: 1px solid rgb(189, 189, 189);
+	padding: 5px;
+`;
+
 const PostContainer = (props) => {
 	//onsole.log('Post Container props: ', props);
 	return (
-		<div className="post">
-
-         <Post 
-            {...props.post}
-          />
+		<PostDiv>
+			<Post {...props.post} />
 
 			<CommentSection comments={props.post.comments} likes={props.post.likes} timestamp={props.post.timestamp} />
-		</div>
+		</PostDiv>
 	);
 };
 
