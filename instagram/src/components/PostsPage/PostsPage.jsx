684d7a28f2ad2f1a3f05e6e2e65d97b8b9ce138a
Sorin Chis <kish.sorin@yahoo.com>
2019-02-21 16:10:21 +0200

    formating PostPage component using Hooks

--- instagram/src/components/PostsPage/PostsPage.jsx
+++ instagram/src/components/PostsPage/PostsPage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import DummyData from '../../dummy-data';
 import './PostsPage.css';
 import PropType from 'prop-types';
@@ -6,82 +6,113 @@ import SearchBar from '../SearchBar/SearchBar';
 import PostContainer from '../PostContainer/PostContainer';
 import styled from 'styled-components';
 
-
 const Header = styled.h1`
-   max-width:40%;
-   margin: 20px auto;
-`
+	max-width: 40%;
+	margin: 20px auto;
+`;
 
+const PostsPage = (props) => {
+	const [ dummyData, setDummyData ] = useState([]);
+	const [ filtredData, setFiltredData ] = useState([]);
+	const [ search, setSearch ] = useState('');
 
-class PostsPage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			dummyData: [],
-			filteredData: [],
-			search: '',
-		};
-	}
+	useEffect(() => {
+      fetch(DummyData)
+         .then(setDummyData(DummyData))
+         .catch((err) => console.log('error fetching data'));
+	});
 
-	componentDidMount() {
-		// this.setState({
-		// 	dummyData: DummyData
-		// }).catch(err=> console.log('Error fa'))
-		console.log('Component did mounth');
-		fetch(DummyData)
-			.then(this.setState({ dummyData: DummyData }))
-			.catch((err) => console.log('error fetching data'));
-	}
+	//console.group(dummyData);
 
-	handleChanges = (event) => {
-		//this.setState({[event.target.name]: event.target.value})
-		this.setState({
-			search: event.target.value
-		});
-		const filtred = this.state.dummyData.filter((post) => {
-			if (post.username.includes(this.state.search)) {
-            //localStorage.setItem('search',post)
+	const handleChanges = (event) => {
+		setSearch(event.target.value);
+
+		const filtred = dummyData.filter((post) => {
+			if (post.username.includes(search)) {
 				return post;
 			} else return null;
 		});
 
-		this.setState({
-			filteredData: filtred
-      });
-      // console.log('Local Storage',localStorage.search)
-		// console.log(this.state.filteredData);
-		// console.log(this.state.filteredData.length);
-
+		setFiltredData(filtred)
 	};
 
-   
-	render() {
-		return (
-			<div>
-				<Header>INSTACLONE PROJECT</Header>
+	return (
+		<div>
+			<Header>INSTACLONE PROJECT</Header>
 
-            <SearchBar 
-               handleChanges={this.handleChanges} 
-               {...this.props}
-            />
-				{this.state.filteredData.length === 0 ? (
-					this.state.dummyData.map((post, i) => {
-						return (
-							<div key={i}>
-                        {/* <h1>No Data Was Found</h1> */}
-								<PostContainer key={i} post={post} />
-							</div>
-						);
-					}) 
-				) : (
-					this.state.filteredData.map((post, i) => {
-						return <PostContainer key={i} post={post} />;
-					})
-				)}
-			</div>
-		);
-	}
-}
+			<SearchBar handleChanges={handleChanges} {...props} />
+			{filtredData.length === 0 ? (
+				dummyData.map((post, i) => {
+					return (
+						<div key={i}>
+							<PostContainer key={i} post={post} />
+						</div>
+					);
+				})
+			) : (
+				filtredData.map((post, i) => {
+					return <PostContainer key={i} post={post} />;
+				})
+			)}
+		</div>
+	);
+};
+
+// class PostsPage extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			dummyData: [],
+// 			filteredData: [],
+// 			search: ''
+// 		};
+// 	}
+
+// 	componentDidMount() {
+// 		console.log('Component did mounth');
+// 	fetch(DummyData)
+// 		.then(this.setState({ dummyData: DummyData }))
+// 		.catch((err) => console.log('error fetching data'));
+// }
+
+// handleChanges = (event) => {
+// 	this.setState({
+// 		search: event.target.value
+// 	});
+// 	const filtred = this.state.dummyData.filter((post) => {
+// 		if (post.username.includes(this.state.search)) {
+// 			return post;
+// 		} else return null;
+// 	});
+
+// 	this.setState({
+// 		filteredData: filtred
+// 	});
+// };
+
+// 	render() {
+// 		return (
+// 			<div>
+// <Header>INSTACLONE PROJECT</Header>
+
+// <SearchBar handleChanges={this.handleChanges} {...this.props} />
+// {this.state.filteredData.length === 0 ? (
+// 	this.state.dummyData.map((post, i) => {
+// 		return (
+// 			<div key={i}>
+// 				<PostContainer key={i} post={post} />
+// 			</div>
+// 		);
+// 	})
+// ) : (
+// 	this.state.filteredData.map((post, i) => {
+// 		return <PostContainer key={i} post={post} />;
+// 	})
+// )}
+// 			</div>
+// 		);
+// 	}
+// }
 
 PostsPage.propTypes = {
 	dummyData: PropType.shape({
